9a4dd62590cdcb8241252f0ab68f69dff9955cd7
Eric Tiedeken <eric.tiedeken@gmail.com>
2020-09-04 11:16:52 -0500

    Update Chrome UserAgent

--- projects/the-application/src/install/install.component.ts
+++ projects/the-application/src/install/install.component.ts
@@ -16,7 +16,7 @@ export class InstallComponent {
       (/iPhone|iPad|iPod/.test(window.navigator.platform) ||
         (window.navigator.maxTouchPoints > 2 &&
           /MacIntel/.test(window.navigator.platform))) &&
-      !/Chrome/.test(window.navigator.userAgent)
+      !/Chrome|CriOS/.test(window.navigator.userAgent)
     )
   }
 
