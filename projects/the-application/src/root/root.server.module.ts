aa59b5b158182928df275565815166b7a91f3672
tedaky <eric.tiedeken@gmail.com>
2020-12-17 02:03:56 +0000

    start locale

--- projects/the-application/src/root/root.server.module.ts
+++ projects/the-application/src/root/root.server.module.ts
@@ -1,15 +1,34 @@
 import { NgModule } from '@angular/core'
-import { ServerModule } from '@angular/platform-server'
+import { TransferState } from '@angular/platform-browser'
+import {
+  ServerModule,
+  ServerTransferStateModule
+} from '@angular/platform-server'
 import { Routes, RouterModule } from '@angular/router'
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core'
 
 import { RootModule } from './root.module'
 import { RootComponent } from './root.component'
 import { ShellComponent } from '../shell/shell.component'
+import { translateServerLoaderFactory } from '../translate-loader/translate-server.loader'
 
 const routes: Routes = [{ path: 'shell', component: ShellComponent }]
 
 @NgModule({
-  imports: [RootModule, ServerModule, RouterModule.forRoot(routes)],
+  imports: [
+    RootModule,
+    RouterModule.forRoot(routes),
+    ServerModule,
+    ServerTransferStateModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: translateServerLoaderFactory,
+        deps: [TransferState]
+      }
+    })
+  ],
   bootstrap: [RootComponent],
   declarations: [ShellComponent]
 })
