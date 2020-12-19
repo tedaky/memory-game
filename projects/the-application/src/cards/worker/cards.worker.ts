e4518c0223c01f420a7a1af191e9be6014b47662
tedaky <eric.tiedeken@gmail.com>
2020-12-19 01:11:30 +0000

    Better organise cards worker
    Closes #7

--- projects/the-application/src/cards/worker/cards.worker.ts
+++ projects/the-application/src/cards/worker/cards.worker.ts
@@ -1,10 +1,7 @@
 /// <reference lib="webworker" />
 
-import { Cards } from '../cards'
-import { Card } from '../../card/card'
-import { ICard } from '../../card/card.d'
+import { post } from './post'
 import { Count, Match } from '../../statistic/statistic.d'
-import { isNullOrUndefined } from '../../utilities/is-null-or-undefined'
 
 addEventListener(
   'message',
@@ -17,45 +14,3 @@ addEventListener(
     post(event.data.count, event.data.match)
   }
 )
-
-post()
-
-/**
- * Post the deck to the main thread.
- *
- * `Count` With Unique Cards Count default of 2
- * `Match` With Amount of Cards to Match default of 2
- */
-function post(): void
-/**
- * Post the deck to the main thread.
- *
- * @param count `Count` Unique Cards Count
- * @param match `Match` Amount of Cards to Match
- */
-function post(count: Count, match: Match): void
-function post(count?: Count, match?: Match): void {
-  let blank: string
-  let cards: Cards
-  let deck: ICard[]
-  let white: string
-
-  if (isNullOrUndefined(count)) {
-    count = 2
-  }
-
-  if (isNullOrUndefined(match)) {
-    match = 2
-  }
-
-  cards = new Cards(count, match)
-  blank = cards.blank
-  white = cards.white
-  deck = Card.toJSON(cards.deck)
-
-  postMessage({
-    blank,
-    deck,
-    white
-  })
-}
---
+++ projects/the-application/src/cards/worker/post.ts
@@ -0,0 +1,45 @@
+import { Cards } from '../cards'
+import { Card } from '../../card/card'
+import { ICard } from '../../card/card.d'
+import { Count, Match } from '../../statistic/statistic.d'
+import { isNullOrUndefined } from '../../utilities/is-null-or-undefined'
+
+/**
+ * Post the deck to the main thread.
+ *
+ * `Count` With Unique Cards Count default of 2
+ * `Match` With Amount of Cards to Match default of 2
+ */
+export function post(): void
+/**
+ * Post the deck to the main thread.
+ *
+ * @param count `Count` Unique Cards Count
+ * @param match `Match` Amount of Cards to Match
+ */
+export function post(count: Count, match: Match): void
+export function post(count?: Count, match?: Match): void {
+  let blank: string
+  let cards: Cards
+  let deck: ICard[]
+  let white: string
+
+  if (isNullOrUndefined(count)) {
+    count = 2
+  }
+
+  if (isNullOrUndefined(match)) {
+    match = 2
+  }
+
+  cards = new Cards(count, match)
+  blank = cards.blank
+  white = cards.white
+  deck = Card.toJSON(cards.deck)
+
+  postMessage({
+    blank,
+    deck,
+    white
+  })
+}
