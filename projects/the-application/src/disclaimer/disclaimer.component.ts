ac67bee233db8efdda53f3747c2f02d9365326c5
tedaky <eric.tiedeken@gmail.com>
2021-01-20 14:16:19 -0600

    add disclaimer

---
+++ projects/the-application/src/disclaimer/disclaimer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+import { LegalBase } from '../legal/legal-base.component'
+
+@Component({
+  selector: 'app-disclaimer',
+  templateUrl: './disclaimer.component.html',
+  styleUrls: ['./disclaimer.component.scss']
+})
+export class DisclaimerComponent extends LegalBase {}
