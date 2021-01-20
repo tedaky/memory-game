f4b96125fd7ac025910e0e6ef74207f7dfb09394
tedaky <eric.tiedeken@gmail.com>
2021-01-20 14:16:36 -0600

    add privacy policy

---
+++ projects/the-application/src/privacy-policy/privacy-policy.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+import { LegalBase } from '../legal/legal-base.component'
+
+@Component({
+  selector: 'app-privacy-policy',
+  templateUrl: './privacy-policy.component.html',
+  styleUrls: ['./privacy-policy.component.scss']
+})
+export class PrivacyPolicyComponent extends LegalBase {}
